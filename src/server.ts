ee3ed940e09d8e17a5be2d2b4a4f81531d35e16b
sroliver202023 <sroliver202023@gmail.com>
2021-11-17 07:23:02 -0300

    Test Jest finished

--- src/server.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { routes } from './routes/index';
-const app = express();
-const PORT = 3333;
-
-app.use(routes);
-
-app.listen(PORT, () => console.log(`Server is running in http://localhost:${PORT}`));
